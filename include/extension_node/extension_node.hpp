038ef7f266ac91cd96d4182be9eaa094bfa2d157
NishizawaEiji <nishizawa@aisl.cs.tut.ac.jp>
2024-07-12 16:35:07 +0900

    [update]loggerの仕様更新

--- include/extension_node/extension_node.hpp
+++ include/extension_node/extension_node.hpp
@@ -5,20 +5,24 @@
 #include <rosgraph_msgs/msg/clock.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <string>
+#include <data_logger_utils/log_publisher.hpp>
 
 namespace common_lib {
   class ExtensionNode : public rclcpp::Node {
   public:
+    data_logger::LogPublisher log_;
     explicit ExtensionNode(const std::string& node_name, const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
         : rclcpp::Node(node_name, options) {
       file_name_ = node_name;
       time_sync_ = false;
+      log_.set_node(this);
     }
 
     explicit ExtensionNode(const std::string& node_name, const std::string& namespace_, const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
         : rclcpp::Node(node_name, namespace_, options) {
       file_name_ = node_name;
       time_sync_ = false;
+      log_.set_node(this);
     }
 
     /**
@@ -85,18 +89,7 @@ namespace common_lib {
      * @param column_names : colmnのname list
      */
     void init_data_logger(const std::string& name, const std::vector<std::string>& column_names) {
-      using namespace std::chrono_literals;
-      init_log_pub_ = create_publisher<std_msgs::msg::String>("/data_logger/init", rclcpp::QoS(10).reliable());
-      log_pub_      = create_publisher<std_msgs::msg::String>("/data_logger/log", rclcpp::QoS(10).reliable());
-      while (init_log_pub_->get_subscription_count() < 1 && rclcpp::ok()) {
-        RCLCPP_WARN(this->get_logger(), "Waiting for data logger to start!");
-        rclcpp::sleep_for(500ms);
-      }
-      std_msgs::msg::String msg;
-      msg.data = name + ",";
-      for (const auto& column_name : column_names)
-        msg.data += column_name + ",";
-      init_log_pub_->publish(msg);
+      log_.init_data_logger(name, column_names);
     }
     /**
      * @brief data_loggerの初期化
@@ -106,28 +99,13 @@ namespace common_lib {
     void init_data_logger(const std::vector<std::string>& column_names) { init_data_logger(file_name_, column_names); }
 
     /**
-     * @brief data_loggerへlogをpublish(ファイル名指定)
-     *
-     * @param name : file name
-     * @param args : 記録する値
-     */
-    template <class... Args>
-    void log(const std::string& name, Args... args) {
-      std_msgs::msg::String msg;
-      msg.data = name + ",";
-      for (auto&& x : {args...})
-        msg.data += std::to_string(x) + ",";
-      log_pub_->publish(msg);
-    }
-
-    /**
      * @brief data_loggerへlogをpublish
      *
      * @param args : 記録する値
      */
     template <class... Args>
     void log(Args... args) {
-      log(file_name_, args...);
+      log_.publish(args...);
     }
 
     /**
@@ -156,8 +134,6 @@ namespace common_lib {
     std::string file_name_;
     std::vector<std::string> mem_strs_;
     std::optional<rclcpp::Time> clock_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr init_log_pub_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr log_pub_;
     rclcpp::Subscription<rosgraph_msgs::msg::Clock>::SharedPtr clock_sub_;
   };
 } // namespace common_lib
